f1a50c989943fcd98894a2aa4fab882456334d91
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-01-01 17:50:09 +0530

    Update root_main.tf

--- root_main.tf
+++ root_main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "ec2" {
   security_groups = [aws_security_group.ec2_sg.name]
 
   tags = {
-    Name    = "Jan1st"
+    Name    = "Jan2nd"
     Version = var.version_name
   }
 }
